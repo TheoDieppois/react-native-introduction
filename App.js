871a7651655ce5f373161db25a51bbf4d169c565
TheoDieppois <theo76.dieppois@gmail.com>
2023-10-25 20:39:35 +0200

    feat: add basic components

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "#fff",
+          alignItems: "center",
+          flex: 1,
+        }}
+      >
+        <Text>Hello world !</Text>
+        <Button title="Click me" onPress={() => alert("Hello world !")} />
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            backgroundColor: "#ccc",
+            width: 200,
+            height: 30,
+          }}
+        />
+
+        <ScrollView>
+          <Image
+            source={require("./assets/dog.jpg")}
+            style={{ width: 200, height: 200 }}
+          />
+          <Image
+            source={require("./assets/dog.jpg")}
+            style={{ width: 200, height: 200 }}
+          />
+          <Image
+            source={require("./assets/dog.jpg")}
+            style={{ width: 200, height: 200 }}
+          />
+          <Image
+            source={require("./assets/dog.jpg")}
+            style={{ width: 200, height: 200 }}
+          />
+          <Image
+            source={{
+              uri: "https://picsum.photos/id/1084/536/354?grayscale",
+            }}
+            style={{ width: 200, height: 200 }}
+            resizeMode="contain"
+          />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "red",
     justifyContent: "center",
   },
 });
