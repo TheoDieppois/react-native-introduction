c19511267db52a8edae9d3190f654a96f51e22ec
TheoDieppois <theo76.dieppois@gmail.com>
2023-10-28 00:15:59 +0200

    fix: remove unnecessary components

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StyleSheet, SafeAreaView } from "react-native";
-import Header from "./components/Header/Header";
-import PostList from "./components/PostList/PostList";
+import { StyleSheet, SafeAreaView, View, Text } from "react-native";
+import Post from "./components/Post/Post";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Header />
-      <PostList />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Exercice 1</Text>
+      </View>
+      <View style={styles.postList}>
+        <Post />
+        <Post />
+      </View>
     </SafeAreaView>
   );
 }
@@ -15,5 +19,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ddd",
+  },
+
+  header: {
+    borderBottomWidth: 1,
+    borderColor: "#000",
+    padding: 10,
+  },
+
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+
+  postList: {
+    flex: 1,
+    alignItems: "center",
+    gap: 40,
+    paddingTop: 30,
   },
 });
