66a2bd6fa4b3048294d80bce8ac445dfd9f5d8be
TheoDieppois <theo76.dieppois@gmail.com>
2023-10-28 21:07:57 +0200

    feat: add bottom tabs

--- navigation.js
+++ navigation.js
@@ -1,36 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Feed from "./screens/Feed";
 import TweetDetailScreen from "./screens/TweetDetailsScreen";
 import Profil from "./screens/Profil";
 import { Pressable, Image } from "react-native";
+import Settings from "./screens/Settings";
+import { Ionicons } from "@expo/vector-icons";
 
 // Stack
 const HomeStack = createNativeStackNavigator();
 
-function HomeStackGroup({ navigation }) {
+function HomeStackGroup() {
   return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen
-        name="Feed"
-        component={Feed}
-        options={{
-          headerLeft: () => (
-            <Pressable onPress={() => navigation.openDrawer()}>
-              <Image
-                source={require("./assets/user.jpg")}
-                style={{
-                  width: 40,
-                  height: 40,
-                  borderRadius: 100,
-                  marginLeft: 15,
-                }}
-              />
-            </Pressable>
-          ),
-        }}
-      />
+    <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+      <HomeStack.Screen name="TabsGroup" component={TabsGroup} />
       <HomeStack.Screen
         name="TweetDetailScreen"
         component={TweetDetailScreen}
@@ -57,6 +42,54 @@ function DrawerGroup() {
       />
       <Drawer.Screen name="Profil" component={Profil} />
     </Drawer.Navigator>
+  );
+}
+
+// Tabs
+const Tab = createBottomTabNavigator();
+
+function TabsGroup({ navigation }) {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "ios-settings-sharp";
+          } else if (route.name === "Notifications") {
+            iconName = focused ? "ios-notifications" : "notifications-outline";
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+
+        tabBarActiveTintColor: "#1DA1F2",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          headerLeft: () => (
+            <Pressable onPress={() => navigation.openDrawer()}>
+              <Image
+                source={require("./assets/user.jpg")}
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 100,
+                  marginLeft: 15,
+                }}
+              />
+            </Pressable>
+          ),
+        }}
+      />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
   );
 }
 
